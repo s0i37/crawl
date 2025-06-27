a857b628cc5334714d616353d2126d017d007b00
s0i37 <s0i37@work>
2025-06-27 16:04:11 +0500

    added more file types; improved save images

--- crawl.sh
+++ crawl.sh
@@ -45,11 +45,17 @@ function escape(){
 	echo -n '"'
 }
 
+function save_image(){
+	path="$1"
+	SIZE='640x480'
+	convert -resize $SIZE "$path" "$IMAGES/${path//\//-}" 2> /dev/null || cp "$path" "$IMAGES/${path//\//-}"
+}
+
 function fork(){
 	tempdir="$1"
 	ln -s "$(realpath $0)" "$tempdir/$(basename $0)"
 	ln -s "$(realpath $index)" "$tempdir/$index"
-	( cd "$tempdir"; "./$(basename $0)" "$where" "${opts[@]}"; )
+	( cd "$tempdir"; "./$(basename $0)" "$where"; )
 }
 
 where="${1//../}"
@@ -99,14 +105,14 @@ do
 			echo $GREEN " [text]" $RESET
 			;;
 		application/msword)
-			echo -n "doc," >> "$index"
+			echo -n "word," >> "$index"
 			catdoc "$path" | escape >> "$index"
-			echo $GREEN " [doc]" $RESET
+			echo $GREEN " [word]" $RESET
 			;;
 		application/vnd.openxmlformats-officedocument.wordprocessingml.document)
-			echo -n "doc," >> "$index"
+			echo -n "word," >> "$index"
 			unzip -p "$path" 2> /dev/null | grep -a '<w:r' | sed 's/<w:p[^<\/]*>/ /g' | sed 's/<[^<]*>//g' | grep -a -v '^[[:space:]]*$' | sed G | escape >> "$index"
-			echo $GREEN " [docx]" $RESET
+			echo $GREEN " [word]" $RESET
 			if unzip -l "$path" | grep -q 'word/media/'; then
 				temp=$(tempfile 2>/dev/null)
 				rm "$temp" && mkdir -p "$temp/$path"
@@ -116,45 +122,129 @@ do
 			fi
 			;;
 		application/vnd.ms-excel)
-			echo -n "xls," >> "$index"
+			echo -n "excel," >> "$index"
 			xls2csv -x "$path" | escape >> "$index"
-			echo $GREEN " [xls]" $RESET
+			echo $GREEN " [excel]" $RESET
 			;;
 		application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)
-			echo -n "xls," >> "$index"
-			#libreoffice --convert-to csv "$path" out.csv
+			echo -n "excel," >> "$index"
 			unzip -p "$path" 2> /dev/null | grep -a -e '<si><t' -e '<vt:lpstr>' | sed 's/<[^<\/]*>/ /g' | sed 's/<[^<]*>//g' | escape >> "$index"
-			echo $GREEN " [xlsx]" $RESET
+			echo $GREEN " [excel]" $RESET
+			if unzip -l "$path" | grep -q 'xl/media/'; then
+				temp=$(tempfile 2>/dev/null)
+				rm "$temp" && mkdir -p "$temp/$path"
+				unzip "$path" 'xl/media/*' -d "$temp/$path" > /dev/null
+				fork "$temp"
+				rm -r "$temp"
+			fi
+			;;
+		application/vnd.openxmlformats-officedocument.presentationml.presentation)
+			echo -n "powerpoint," >> "$index"
+			unzip -qc "$path" 'ppt/slides/slide*.xml' 2> /dev/null | grep -oP '(?<=\<a:t\>).*?(?=\</a:t\>)' | escape >> "$index"
+			echo $GREEN " [powerpoint]" $RESET
+			if unzip -l "$path" | grep -q 'ppt/media/'; then
+				temp=$(tempfile 2>/dev/null)
+				rm "$temp" && mkdir -p "$temp/$path"
+				unzip "$path" 'ppt/media/*' -d "$temp/$path" > /dev/null
+				fork "$temp"
+				rm -r "$temp"
+			fi
+			;;
+		application/vnd.oasis.opendocument.graphics|application/vnd.ms-visio.drawing.main*)
+			echo -n "visio," >> "$index"
+			#LibreOffice
+			unzip -qc "$path" 'content.xml' 2> /dev/null | grep -oP '(?<=\<text:p\>).*?(?=\</text:p\>)' | escape >> "$index"
+			#Microsoft
+			for page in $(unzip -qq -l "$path" 'visio/pages/*.xml' | awk '{print $NF}'); do
+				unzip -qc "$path" "$page" | xq | grep -e '"#text":' -e '"@Name":' | cut -d : -f 2- | tr -d '"'
+			done | escape >> "$index"
+			echo $GREEN " [visio]" $RESET
+			#LibreOffice
+			if unzip -l "$path" | grep 'Pictures/' | grep -q -v 'TablePreview1.svm'; then
+				temp=$(tempfile 2>/dev/null)
+				rm "$temp" && mkdir -p "$temp/$path"
+				unzip "$path" 'Pictures/*.jpg' -d "$temp/$path" > /dev/null
+				fork "$temp"
+				rm -r "$temp"
+			fi
+			#Microsoft
+			if unzip -l "$path" | grep -q 'visio/media/'; then
+				temp=$(tempfile 2>/dev/null)
+				rm "$temp" && mkdir -p "$temp/$path"
+				unzip "$path" 'visio/media/*' -d "$temp/$path" > /dev/null
+				fork "$temp"
+				rm -r "$temp"
+			fi
 			;;
 		application/pdf)
 			echo -n "pdf," >> "$index"
 			pdf2txt -t text "$path" 2> /dev/null | escape >> "$index"
 			echo $GREEN " [pdf]" $RESET
+			if [ $(pdfimages -list "$path" | tail -n +3 | wc -l) -ge 1 ]; then
+				temp=$(tempfile 2>/dev/null)
+				rm "$temp" && mkdir -p "$temp/$path"
+				pdfimages -all "$path" "$temp/$path/img"
+				fork "$temp"
+				rm -r "$temp"
+			fi
+			;;
+		application/x-ms-shortcut)
+			echo -n "lnk," >> "$index"
+			lnkinfo "$path" 2> /dev/null | grep -e 'String' | cut -d ' ' -f 2- | escape >> "$index"
+			echo $GREEN " [lnk]" $RESET
 			;;
 		application/x-executable|application/*microsoft*-executable|application/x*dos*)
 			echo -n "executable," >> "$index"
 			rabin2 -qq -z "$path" 2> /dev/null | escape >> "$index"
 			echo $GREEN " [exe]" $RESET
 			;;
-		application/x-object|application/x-sharedlib|application/x-executable)
+		application/x-object|application/x-sharedlib|application/x-executable|application/x-pie-executable)
 			echo -n "executable," >> "$index"
 			rabin2 -qq -z "$path" 2> /dev/null | escape >> "$index"
 			echo $GREEN " [elf]" $RESET
 			;;
 		image/*)
+			LANGS=(eng rus)
 			echo -n "image," >> "$index"
-			#identify -verbose "$path" 2> /dev/null | escape >> "$index"
-			tesseract "$path" stdout -l eng 2> /dev/null | escape >> "$index"
-			tesseract "$path" stdout -l rus 2> /dev/null | escape >> "$index"
+			identify -verbose "$path" 2> /dev/null | grep -e 'Geometry:' -e 'User Comment:' | escape >> "$index"
+			for lang in ${LANGS[*]}; do
+				tesseract "$path" stdout -l $lang 2> /dev/null | escape >> "$index"
+			done
+			[ -n "$IMAGES" ] && save_image "$path"
 			#curl -s http://10.250.153.11/string_api -F "file=@$path" | escape >> "$index"
 			echo $GREEN " [image]" $RESET
 			;;
 		audio/*)
+			LANGS=(en-us ru)
 			echo -n "audio," >> "$index"
-			vosk-transcriber --lang en-us --input "$path" 2> /dev/null | escape >> "$index"
+			ffmpeg -i "$path" 2>&1 | grep -e 'Duration:' | escape >> "$index"
+			for lang in ${LANGS[*]}; do
+				vosk-transcriber --lang $lang --input "$path" 2> /dev/null | escape >> "$index"
+			done
 			echo $GREEN " [audio]" $RESET
 			;;
-		application/*compressed*|application/*zip*|application/*rar*|application/*tar*|application/*gzip*|application/*-msi|*/java-archive)
+		video/*)
+			FPS=1
+			echo -n "video," >> "$index"
+			ffmpeg -i "$path" 2>&1 | grep -e 'Duration:' -e 'Stream' | escape >> "$index"
+			echo $GREEN " [video]" $RESET
+			temp=$(tempfile 2>/dev/null)
+			rm "$temp" && mkdir -p "$temp/$path"
+			ffmpeg -i "$path" -acodec copy "$temp/$path/audio.aac" 1> /dev/null 2> /dev/null
+			ffmpeg -i "$path" -vf fps=$FPS "$temp/$path/frame%d.png" 1> /dev/null 2> /dev/null
+			fork "$temp"
+			rm -r "$temp"
+			;;
+		application/x-ole-storage)
+			echo -n "thumbsdb," >> "$index"
+			echo $GREEN " [thumbsdb]" $RESET
+			temp=$(tempfile 2>/dev/null)
+			rm "$temp" && mkdir -p "$temp/$path"
+			vinetto "$path" -o "$temp/$path" 1> /dev/null 2> /dev/null
+			fork "$temp"
+			rm -r "$temp"
+			;;
+		application/*compressed*|application/*zip*|application/*rar*|application/*tar*|application/*gzip*|application/*-msi|*/java-archive|application/x-archive)
 			echo -n "archive," >> "$index"
 			7z l -p '' "$path" 2> /dev/null | tail -n +13 | escape >> "$index"
 			echo $GREEN " [archive]" $RESET
@@ -163,6 +253,43 @@ do
 			7z x -p '' "$path" -o"$temp/$path" 1> /dev/null 2> /dev/null
 			fork "$temp"
 			rm -r "$temp"
+			;;
+		application/x-installshield)
+			echo -n "archive," >> "$index"
+			echo $GREEN " [cab]" $RESET
+			temp=$(tempfile 2>/dev/null)
+			rm "$temp" && mkdir -p "$temp/$path"
+			cabextract -d "$temp/$path" "$path" 1> /dev/null 2> /dev/null
+			fork "$temp"
+			rm -r "$temp"
+			;;
+		application/x-rpm)
+			echo -n "package," >> "$index"
+			echo $GREEN " [rpm]" $RESET
+			temp=$(tempfile 2>/dev/null)
+			rm "$temp" && mkdir -p "$temp/$path"
+			rpm2cpio "$path" | cpio -idmuv --no-absolute-filenames -p "$temp/$path" 1> /dev/null 2> /dev/null
+			fork "$temp"
+			rm -r "$temp"
+			;;
+		application/vnd.debian.binary-package)
+			echo -n "package," >> "$index"
+			echo $GREEN " [deb]" $RESET
+			temp=$(tempfile 2>/dev/null)
+			rm "$temp" && mkdir -p "$temp/$path"
+			dpkg --extract "$path" "$temp/$path" 1> /dev/null 2> /dev/null
+			fork "$temp"
+			rm -r "$temp"
+			;;
+		application/x-bytecode.python)
+			echo -n "bytecode," >> "$index"
+			pycdc "$path" 2> /dev/null | tail -n +5 | escape >> "$index"
+			echo $GREEN " [bytecode]" $RESET
+			;;
+		application/x-ms-evtx)
+			echo -n "winevent," >> "$index"
+			evtx "$path" -o jsonl | escape >> "$index"
+			echo $GREEN " [evtx]" $RESET
 			;;
 		application/vnd.ms-outlook)
 			echo -n "message," >> "$index"
@@ -188,6 +315,11 @@ do
 			fork "$temp"
 			rm -r "$temp"
 			;;
+		application/*sqlite3)
+			echo -n "sqlite," >> "$index"
+			sqlite3 "$path" '.dump' 2> /dev/null | escape >> "$index"
+			echo $GREEN " [sqlite]" $RESET
+			;;
 		*.tcpdump.pcap)
 			echo -n "pcap," >> "$index"
 			tcpdump -r "$path" -nn -A | escape >> "$index"
@@ -200,9 +332,12 @@ do
 			;;
 		application/octet-stream)
 			echo -n "raw," >> "$index"
-			#strings "$path" | escape >> "$index"
-			echo -n "" >> "$index"
+			temp=$(tempfile 2>/dev/null)
+			rm "$temp" && mkdir -p "$temp/$path"
+			binwalk -e "$path" -C "$temp/$path" | tail -n +4 | escape >> "$index"
 			echo $GREEN " [raw]" $RESET
+			fork "$temp"
+			rm -r "$temp"
 			;;
 		*)
 			file "$path" | grep text > /dev/null &&
